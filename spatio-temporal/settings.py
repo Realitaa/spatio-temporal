b80a61072af4613045e03f214a0c7e6a5ce8d637
Realitaa <rezamuliaputra927@gmail.com>
2026-04-07 17:47:00 +0700

    feat: freezing dependencies

--- spatio-temporal/settings.py
+++ spatio-temporal/settings.py
@@ -29,7 +29,10 @@ env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY", default="django-insecure-3472^sitrt^-xx#!u-t_4bi2ac5&6^i8%0s9!3!zyy#uf#rq44")
+SECRET_KEY = env(
+    "SECRET_KEY",
+    default="django-insecure-3472^sitrt^-xx#!u-t_4bi2ac5&6^i8%0s9!3!zyy#uf#rq44",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=True)
@@ -61,7 +64,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    
     "inertia.middleware.InertiaMiddleware",
     "spatio-temporal.middleware.DataShareMiddleware",
 ]
@@ -79,7 +81,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                
             ],
         },
     },
@@ -96,25 +97,23 @@ if "DATABASE_URL" in env:
 else:
     DATABASES = {
         "default": {
-            
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
             "OPTIONS": {
-                "transaction_mode": "IMMEDIATE",
+                # "transaction_mode": "IMMEDIATE",
                 "timeout": 5,  # seconds
-                "init_command": """
-                    PRAGMA foreign_keys = ON;
-                    PRAGMA journal_mode = WAL;
-                    PRAGMA synchronous = NORMAL;
-                    PRAGMA temp_store = MEMORY;
-                    PRAGMA mmap_size = 134217728;
-                    PRAGMA journal_size_limit = 67108864;
-                    PRAGMA cache_size = 2000;
-                """,
+                # "init_command": """
+                #     PRAGMA foreign_keys = ON;
+                #     PRAGMA journal_mode = WAL;
+                #     PRAGMA synchronous = NORMAL;
+                #     PRAGMA temp_store = MEMORY;
+                #     PRAGMA mmap_size = 134217728;
+                #     PRAGMA journal_size_limit = 67108864;
+                #     PRAGMA cache_size = 2000;
+                # """,
             },
-            
         }
-}
+    }
 
 
 # Password validation
@@ -138,11 +137,9 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     "django.contrib.auth.backends.ModelBackend",
-    
 ]
 
 LOGIN_REDIRECT_URL = "/"
-
 
 
 # Internationalization
@@ -188,9 +185,11 @@ INERTIA_JSON_ENCODER = inertia_settings.INERTIA_JSON_ENCODER
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+
 # http://whitenoise.evans.io/en/stable/django.html#WHITENOISE_IMMUTABLE_FILE_TEST
 def immutable_file_test(path, url):
     # Match vite (rollup)-generated hashes, à la, `some_file-CSliV9zW.js`
     return re.match(r"^.+[.-][0-9a-zA-Z_-]{8,12}\..+$", url)
+
 
 WHITENOISE_IMMUTABLE_FILE_TEST = immutable_file_test
